f021f657670730fc7b3c92dfbe217cb73cf89bbd
rainbief <rainbief@gmail.com>
2025-03-24 01:52:01 -0700

    activity

---
+++ activities Function - Activity/script.js
@@ -0,0 +1,13 @@
+function whatShalliWear (temp) {
+    if (temp < 60) {
+        console.log ("Wear a Jacket");
+    } else if (temp < 70) {
+        console.log ("Wear a Sweater");
+    } else {
+        console.log("Wear a t-shirt")
+    }
+}
+
+whatShalliWear(50)
+whatShalliWear(60)
+whatShalliWear(80)
