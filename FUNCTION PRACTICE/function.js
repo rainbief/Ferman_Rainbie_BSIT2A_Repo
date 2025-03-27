310c5cf821417543785bf386389d73047a113536
rainbief <rainbief@gmail.com>
2025-03-27 17:16:45 +0800

    Function practice

---
+++ FUNCTION PRACTICE/function.js
@@ -0,0 +1,7 @@
+function printName(firstName, lastName) {
+    var fullname = firstName + ' ' + lastName;
+    return fullname;
+}
+var name = "Rainbie";
+var last = "Ferman";
+console.log('My first name is:' , printName(name, last))
